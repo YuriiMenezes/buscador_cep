db82c7ae8d20b41efa8962275513ddd7957f610f
YuriiMenezes <yurythc@gmail.com>
2023-02-14 15:06:00 -0300

    first

---
+++ buscador/src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className="container">
+
+      <h1 className="title"></h1>
+
+        <div className="containerInput">
+          <input 
+          type="text"
+          placeholder="Digite Seu CEP ..."
+          
+          />
+          <button className="buttonSearch">
+            Procurar
+          </button>
+
+        </div> 
+
+    </div>
+  );
+}
+
+export default App;
